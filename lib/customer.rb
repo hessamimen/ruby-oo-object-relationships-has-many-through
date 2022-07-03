a52c84cb4d4503de6076b24f815d0b580f0b2cbe
Hessam Imen <hessam.imen@gmail.com>
2022-07-03 21:37:38 +1000

    Done with assignment

--- lib/customer.rb
+++ lib/customer.rb
@@ -26,3 +26,16 @@ class Customer
   end
   
   end
+
+#   IRB
+#     require_relative './lib/customer.rb'
+#     require_relative './lib/meal.rb'
+#     require_relative './lib/waiter.rb'
+
+#     sam = Customer.new("Sam", 27)
+#     pat = Waiter.new("Pat", 2)
+#     alex = Waiter.new("Alex", 5)
+  
+#     sam.new_meal(pat, 50, 10) # A Customer creates a Meal, passing in a Waiter instance
+#     sam.new_meal(alex, 20, 3) # A Customer creates a Meal, passing in a Waiter instance
+#     pat.new_meal(sam, 30, 5) # A Waiter creates a Meal, passing in a Customer instance
